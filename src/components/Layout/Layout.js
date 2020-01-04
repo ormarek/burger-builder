2fdf38a90b63a4fa34e6a1ef7569b393a96f0db0
Marek Orlowski <marek.orlowski@hotmail.com>
2020-01-04 16:22:53 +0100

    Not named commit

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Auxiliary from "../../hoc/Auxiliary";
+import classes from "./Layout.module.scss";
 
 const Layout = props => (
   <Auxiliary>
     {/* TODO: Toolbar, SideDrawer, Backdrop components */}
     <div>Toolbar, SideDrawer, Backdrop</div>
-    <main>{props.children}</main>
+    <main className={classes.pageBody}>{props.children}</main>
   </Auxiliary>
 );
 
