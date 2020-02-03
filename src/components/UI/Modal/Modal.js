c47249486efc7bc51740d487ab94e2fbdded5896
Marek Orłowski <ormarek@users.noreply.github.com>
2020-02-03 12:34:07 +0100

    Lecture 168

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,6 +1,10 @@
 import React from "react";
 import classes from "./Modal.module.scss";
 
-const modal = props => <div className={classes.Modal}>{props.children}</div>;
+const modal = props => (
+  <div className={`${classes.Modal} ${props.show ? classes.Active : ""}`}>
+    {props.children}
+  </div>
+);
 
 export default modal;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -21,7 +21,8 @@ class BurgerBuilder extends Component {
       meat: 0
     },
     totalPrice: 4,
-    purchasable: false
+    purchasable: false,
+    purchasing: false
   };
 
   updatePurchaseState(ingredients) {
@@ -66,6 +67,10 @@ class BurgerBuilder extends Component {
     this.updatePurchaseState(updatedIngredients);
   };
 
+  purchaseHandler = () => {
+    this.setState({ purchasing: true });
+  };
+
   render() {
     const disabledInfo = {
       ...this.state.ingredients
@@ -75,7 +80,7 @@ class BurgerBuilder extends Component {
     }
     return (
       <Auxiliary>
-        <Modal>
+        <Modal show={this.state.purchasing}>
           <OrderSummary ingredients={this.state.ingredients} />
         </Modal>
         <Burger ingredients={this.state.ingredients} />
@@ -85,6 +90,7 @@ class BurgerBuilder extends Component {
           disabled={disabledInfo}
           purchasable={this.state.purchasable}
           price={this.state.totalPrice}
+          ordered={this.purchaseHandler}
         />
       </Auxiliary>
     );
