b6b71a51f1563968ccc346072a0d9091e4b7267a
Marek Orłowski <ormarek@users.noreply.github.com>
2020-02-03 12:54:32 +0100

    Lecture 169

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,10 +1,15 @@
 import React from "react";
 import classes from "./Modal.module.scss";
+import Auxiliary from "../../../hoc/Auxiliary";
+import Backdrop from "../Backdrop/Backdrop";
 
 const modal = props => (
-  <div className={`${classes.Modal} ${props.show ? classes.Active : ""}`}>
-    {props.children}
-  </div>
+  <Auxiliary>
+    <Backdrop show={props.show} clicked={props.modalClosed}></Backdrop>
+    <div className={`${classes.Modal} ${props.show ? classes.Active : ""}`}>
+      {props.children}
+    </div>
+  </Auxiliary>
 );
 
 export default modal;
